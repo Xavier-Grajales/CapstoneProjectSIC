de3af6f891ee6091e03545956a62c6cf48593cff
Xavier-Grajales <2193037680@correo.ler.uam.mx>
2021-12-21 21:21:59 -0600

    librerias_wifi
    en esta parte se agregan las librerias necesarias para establecer la conectividad wifi

--- sistema_monitoreo_tercera_edad/sistema_monitoreo_tercera_edad.ino
+++ sistema_monitoreo_tercera_edad/sistema_monitoreo_tercera_edad.ino
@@ -11,6 +11,8 @@
 /*Bibliotecas necesarias para la ejecución del programa*/
 #include <Wire.h>
 #include <Adafruit_MLX90614.h>
+#include <WiFi.h>  // Biblioteca para el control de WiFi
+#include <PubSubClient.h> //Biblioteca para conexion MQTT
 /*Definimos los pines necesarios para el sensor MLX90614
     SensorMLX90614------PinESP32CAM
     Vin-----------------3.3v
@@ -20,12 +22,26 @@
 */
 #define I2C_SDA 14
 #define I2C_SCL 15
+//---------------------------Conectividad---------------------------------------------------
+//Datos de WiFi
+const char* ssid = "SUPERCABLE_6715";  // Aquí debes poner el nombre de tu red
+const char* password = "V19010404641";  // Aquí debes poner la contraseña de tu red
+
+//Datos del broker MQTT
+const char* mqtt_server = "3.122.36.163"; // Si estas en una red local, coloca la IP asignada, en caso contrario, coloca la IP publica
+IPAddress server(3,122,36,163);
+
+// Objetos
+WiFiClient espClient; // Este objeto maneja los datos de conexion WiFi
+PubSubClient client(espClient); // Este objeto maneja los datos de conexion al broker
+
+//-------------------Data sensors-----------------------------------------------------------------
 /*creamos la instancia i2c*/
 TwoWire I2CSensors = TwoWire(0);
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
 /*Declaración de las variables que nos serviran como temporizadores*/
-unsigned long TimeNow=0;
+unsigned long TimeNow=0;//Almacenaremos el tiempo en ms
 unsigned long PreviousTime_MLX90614=0;
 /*Declaración de las variables que nos permiten determinar el tiempo de retardo para ejecutar 
 cada acción simulando un delay*/
@@ -37,7 +53,6 @@ void setup() {
  mlx.begin();
  /*Inicializamos los pines del ESP32CAM como SDA y SCL*/
  I2CSensors.begin(I2C_SDA, I2C_SCL, 100000);
-    
 }
 
 void loop() {
@@ -45,6 +60,7 @@ void loop() {
  MLX90614();//llamamos a la función del sensor de temperatura
  
 }
+
 /*Generamos las funciones de cada sensor para tener un código más ordenado */
 void MLX90614(){
   if((TimeNow-PreviousTime_MLX90614)>=Time_MLX90614){
